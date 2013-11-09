6f73858a3fbd7428ae357c634123eb5744fa42b0
Billy Everyteen <ginaliotta@gmail.com>
2013-11-08 20:16:53 -0500

    added README file

---
+++ test_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '65e3a9b02afd4ae6238af464c2617fb84a545534769f644995bd78accc363cd556a1f2a5b515ee04a169d3035dce5d98c3813b880ceb4d3aad0420ba7e5b9ae5'
